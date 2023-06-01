121999020876545e45ef9ff597a22168374b68e1
Mwalima <m.peltenburg@hotmail.com>
2023-06-01 09:59:09 +0200

    wroncredentials pane reformatted

--- src/main/java/org/monitor/View.java
+++ src/main/java/org/monitor/View.java
@@ -3,6 +3,7 @@ package org.monitor;
 import org.monitor.model.User;
 import org.monitor.view.ClosingView;
 import org.monitor.view.RegistrationView;
+import org.monitor.view.WrongCredentialsView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -30,7 +31,6 @@ public class View extends JFrame implements ActionListener, KeyListener, MouseLi
     public View() {
         loginPanel();
         monitoringPanel();
-        wrongCredentialsPanel();
     }
 
     /**
@@ -189,54 +189,26 @@ public class View extends JFrame implements ActionListener, KeyListener, MouseLi
         return monitoringBorderPanel;
     }
 
-   private JPanel wrongCredentialsPanel() {
-        welkomeText = new JLabel();
-        String logintext = "<html><p>Vul AUB de correcte gegevens in.</p></html>";
-        welkomeText.setText(logintext);
-        welkomeText.setFont(new Font("Arial", Font.BOLD, 20));
-        welkomeText.setForeground(new Color(245, 239, 239));
-        welkomeText.setBounds(200, 28, 450, 200);
 
-        wroncredentialspanel = new JPanel();
-        wroncredentialspanel.setLayout(null);
-        wroncredentialspanel.add(welkomeText);
-        wroncredentialspanel.setBackground(new Color(40, 31, 107));
-
-        returnbutton = new JButton("return");
-        returnbutton.setBounds(200, 310, 120, 25);
-        returnbutton.setBackground(new Color(209, 31, 61));
-        returnbutton.setForeground(new Color(191, 191, 191));
-        wroncredentialspanel.add(returnbutton);
-        add(wroncredentialspanel, BorderLayout.CENTER);
-
-        returnbutton.addActionListener(this);
-        return wroncredentialspanel;
-    }
     @Override
     public void actionPerformed(ActionEvent e) {
         View view = new View();
         ClosingView closingView = new ClosingView();
         RegistrationView registration = new RegistrationView();
+        WrongCredentialsView wrongcredentials = new WrongCredentialsView();
         User user = new User();
 
-        /*
-        TODO  get user entered username from the usernameText
-        String passValue = passwordText.getText();
-         */
         if (e.getSource() == loginbutton) {
             try {
+                //get input from form
                 emailValue = usernameText.getText();
                 passwordValue = passwordText.getText();
 
-                //add values to arrylist
-                ArrayList<String>loginvalues = new ArrayList<String>();
-                loginvalues.add(emailValue);
-                loginvalues.add(passwordValue);
+                //add values to user object
+                user.setEmailadress(emailValue);
+                user.setPassword(passwordValue);
 
-                System.out.println(emailValue);
-                System.out.println(passwordValue);
-
-                if (loginvalues.get(0).equals(user.getUserData(emailValue))) {
+                if (user.getUserData() == 1) {
                     //if authentic, navigate user to a new page
                     gameFrame(view.loginPanel()).dispose();
                     view.gameFrame(view.monitoringPanel());
@@ -246,21 +218,16 @@ public class View extends JFrame implements ActionListener, KeyListener, MouseLi
                 } else {
                     //close previouos frame
                     gameFrame(view.loginPanel()).dispose();
-                    view.gameFrame(view.wrongCredentialsPanel());
+                    view.gameFrame(wrongcredentials.wrongCredentialsPanel());
                 }
             } catch (Exception ea) {
                 //close previouos frame
                 gameFrame(view.loginPanel()).dispose();
-                view.gameFrame(view.wrongCredentialsPanel());
+                view.gameFrame(wrongcredentials.wrongCredentialsPanel());
             }
         }
         if (e.getSource() == exitbutton) {
             view.gameFrame(closingView.closePanel());
-        }
-        if (e.getSource() == returnbutton) {
-            //close previouos frame
-            gameFrame(view.wrongCredentialsPanel()).dispose();
-            view.gameFrame(view.loginPanel());
         }
     }
 
